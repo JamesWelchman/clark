0b0f22cb768d08ee300a806d8a7a82c9688e4665
James Welchman <james.welchman@gmail.com>
2019-06-07 15:46:46 +0100

    Created pacman package

---
+++ pkg/pacman/pacman.go
@@ -0,0 +1,57 @@
+/*
+pacman counts the number of updates to install
+
+  numUpdates, err := pacman.NumUpdates()
+
+numUpdates is an int
+*/
+package pacman
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+var cmd = [...]string{
+	"/usr/bin/sudo",
+	"pacman",
+	"-Qu",
+}
+
+func runPacman() (string, error) {
+	writer := &strings.Builder{}
+
+	cmd := exec.Cmd{
+		Path:   cmd[0],
+		Args:   cmd[:],
+		Stdout: writer,
+	}
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return writer.String(), nil
+}
+
+func countUpdates(output string) int {
+	total := 0
+
+	for _, r := range output {
+		if r == '\n' {
+			total++
+		}
+	}
+
+	return total
+}
+
+func NumUpdates() (int, error) {
+	output, err := runPacman()
+	if err != nil {
+		return 0, fmt.Errorf("couldn't run pacman :: %v", err)
+	}
+
+	return countUpdates(output), nil
+}
